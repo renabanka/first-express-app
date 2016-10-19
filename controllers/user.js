9df416ff98f52e1aa1a7f27421589b58fa182f31
Rena Banka <renabanka@Renas-MacBook-Pro.local>
2016-10-19 12:27:08 -0500

    userupdate

--- controllers/user.js
+++ controllers/user.js
@@ -29,4 +29,17 @@ ctrl.get('/', function(req, res, next) {
 	res.json(users);
 });
 
+
+ctrl.get('/1', function(req, res, next) {
+	res.json(users[0]);
+});
+
+ctrl.get('/2', function(req, res, next) {
+	res.json(users[1]);
+});
+
+ctrl.get('/3', function (req,res, next) {
+	res.json(users[2]);
+});
+
 module.exports = ctrl;
