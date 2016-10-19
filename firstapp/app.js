134771247f71025f883ae4778fd05d3029b37a76
Rena Banka <renabanka@Renas-MacBook-Pro.local>
2016-10-19 12:14:34 -0500

    all controller are done

--- firstapp/app.js
+++ firstapp/app.js
@@ -4,6 +4,22 @@ var app = express();
 var port = 3000;
 
 
+//set public, views, and handlebars
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'hbs');
+
+//require our controllers
+var homeCtrl = require('./controllers/home');
+var playgroundCtrl = require('./controllers/playground');
+var userCtrl = require('./controllers/user');
+
+
+//then we map '/' routes to controllers
+app.use('/', homeCtrl);
+app.use('/play', playgroundCtrl);
+app.use('/user', userCtrl);
+
 
 
 app.listen(process.env.PORT || port);
